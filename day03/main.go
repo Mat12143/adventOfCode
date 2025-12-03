3acbb9da32c420dee311799fe8279568a7de77b6
Mat12143 <matteo.peretto@studenti.unitn.it>
2025-12-03 13:55:33 +0100

    add: solutions for day3

--- day03/main.go
+++ day03/main.go
@@ -1,11 +1,72 @@
 package day03
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"slices"
+	"strings"
+
+	"github.com/Mat12143/adventOfCode2025/utils"
+)
+
+type Battery struct {
+	jolt int
+	pos  int
+}
 
 func Part1() {
-    fmt.Println("Day 03 Part 1")
+	data := utils.ReadFileByLine("day03/input.txt")
+
+	sum := 0
+
+	for l := range strings.SplitSeq(data, "\n") {
+		if len(l) == 0 {
+			continue
+		}
+
+		numbers := []int{}
+
+		for _, d := range l {
+			numbers = append(numbers, int(d-'0'))
+		}
+		sum += getMaxJoltage(numbers, 2)
+
+	}
+	fmt.Printf("sum: %v\n", sum)
+}
+
+func getMaxJoltage(list []int, dim int) int {
+	if dim == 0 {
+		return 0
+	}
+
+	maxValue := slices.Max(list[:len(list)-dim+1])
+	maxIndex := slices.Index(list, maxValue)
+
+	return (int(math.Pow10(dim-1)) * maxValue) + getMaxJoltage(list[maxIndex+1:], dim-1)
+
 }
 
 func Part2() {
-    fmt.Println("Day 03 Part 2")
+	sum := 0
+
+	data := utils.ReadFileByLine("day03/input.txt")
+
+	for l := range strings.SplitSeq(data, "\n") {
+
+		if len(l) == 0 {
+			continue
+		}
+		numbers := []int{}
+
+		for _, d := range l {
+			numbers = append(numbers, int(d-'0'))
+		}
+
+		test := getMaxJoltage(numbers, 12)
+		sum += test
+
+	}
+
+	fmt.Printf("sum: %v\n", sum)
 }
