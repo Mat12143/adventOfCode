f624ad910dadf44d7abf39d75b341fbe855ae086
Mat12143 <matteo.peretto@studenti.unitn.it>
2025-12-09 13:13:23 +0100

    add: day09 part 1 solution

--- 2025/day09/main.go
+++ 2025/day09/main.go
@@ -1,9 +1,52 @@
 package day09
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	x, y int
+}
+
+func getRectArea(a, b Point) int {
+	return (int(math.Abs(float64(a.x-b.x))) + 1) * (int(math.Abs(float64(a.y-b.y))) + 1)
+}
+
 func Part1(data string) {
-    // TODO: implement Part1
+
+	points := []Point{}
+
+	for l := range strings.SplitSeq(data, "\n") {
+
+		if len(l) == 0 {
+			continue
+		}
+
+		numbers := strings.Split(l, ",")
+		x, _ := strconv.Atoi(numbers[0])
+		y, _ := strconv.Atoi(numbers[1])
+
+		points = append(points, Point{x, y})
+	}
+
+	maxArea := 0
+
+	for _, p := range points {
+		for _, p2 := range points {
+			if p == p2 {
+				continue
+			}
+			maxArea = max(maxArea, getRectArea(p, p2))
+		}
+	}
+
+	fmt.Printf("maxArea: %v\n", maxArea)
+
 }
 
 func Part2(data string) {
-    // TODO: implement Part2
+	// TODO: implement Part2
 }
